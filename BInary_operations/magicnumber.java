fa1e6a7d4ca51386cdfe3b9b0378a16ad0ff55e1
harsshgpt <harshgupta1752003@gmail.com>
2025-03-14 16:08:10 +0530

    magic number using binary

---
+++ BInary_operations/magicnumber.java
@@ -0,0 +1,19 @@
+package BInary_operations;
+
+public class magicnumber {
+    public static void main(String[] args) {
+        int n = 1;
+        int ans  = 0;
+        int base = 5;
+        while(n>0){
+            int last = n&1;
+            n = n>>1;
+
+            ans += last*base;
+
+            base = base*5;
+            
+        }
+        System.out.println(ans);
+    }
+}
