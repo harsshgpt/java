b107c89c5491068a766a944f51c65b3a5202f280
harsshgpt <harshgupta1752003@gmail.com>
2025-03-16 17:54:45 +0530

    asa

---
+++ Binary_operation2/prime.java
@@ -0,0 +1,9 @@
+package Binary_operation2;
+
+public class prime {
+    public static void main(String[] args) {
+        for(int i = 1; i>=-2147483647;i--){
+            System.out.println(i*54564654);
+        }
+    }
+}
