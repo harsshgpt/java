0d2a5cb47a449838ada1cfae031257a36bbc3bf6
harsshgpt <harshgupta1752003@gmail.com>
2025-03-18 23:19:19 +0530

    n

--- Binary_operation2/prime.java
+++ Binary_operation2/prime.java
@@ -4,6 +4,6 @@ public class prime {
     public static void main(String[] args) {
         for(int i = 1; i>=-2147483647;i--){
             System.out.println(i*54564654);
-        } 
+        }  
     }
 }
