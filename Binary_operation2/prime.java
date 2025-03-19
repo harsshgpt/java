e9770e4fca445af2c53cf00e307133fdf562256a
harsshgpt <harshgupta1752003@gmail.com>
2025-03-19 17:12:50 +0530

    re

--- Binary_operation2/prime.java
+++ Binary_operation2/prime.java
@@ -1,9 +1,15 @@
 package Binary_operation2;
-
+import java.util.*;
 public class prime {
     public static void main(String[] args) {
-        for(int i = 1; i>=-2147483647;i--){
-            System.out.println(i*54564654);
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt();
+        System.out.println("The all factors of "+n+ " 36is");
+        for(int i = 2; i <= n/2;i++){
+            
+            if(n%i==0){
+               
+            System.out.println(i);}
         }  
     }
 }
