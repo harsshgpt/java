31067da4423ae9ac7fd918c065dbd05ef209ac2c
harsshgpt <harshgupta1752003@gmail.com>
2025-03-19 17:24:22 +0530

    dfg

--- Binary_operation2/prime.java
+++ Binary_operation2/prime.java
@@ -4,10 +4,10 @@ public class prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n  = sc.nextInt();
-        System.out.println("The all factors of "+n+ " 36is");
-        for(int i = 2; i <= n/2;i++){
+        System.out.println("The all prime numbers till " + n +" is =>");
+        for(int i = 2; i <= n;i++){
             
-            if(n%i==0){
+            if(n%i!=0 || i==2){
                
             System.out.println(i);}
         }  
