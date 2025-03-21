5751e317829f5816731d8f7b4a1cee41b2bec712
harsshgpt <harshgupta1752003@gmail.com>
2025-03-21 16:23:41 +0530

    gdf

--- Binary_operation2/sqrt.java
+++ Binary_operation2/sqrt.java
@@ -2,24 +2,35 @@ package Binary_operation2;
 
 public class sqrt {
     public static void main(String[] args) {
-        int n = 49;
-        System.out.println(square(n));
+        int n = 75;
+        int p = 3;
+        System.out.printf("%.3f",square(n,p));
     }
-    static int square(int n){
+    //O(logn)
+    static double square(int n,int p ){
        int start = 0 ;
        int end = n;
+       double root = 0.0;
        while(start<=end){
         int mid = (start + end)/2;
         if(mid*mid == n){
-            return mid;
+            root =  mid;
+            return root;
         }
         if(mid*mid < n){
             start = mid+1;
-        }
-        if(mid*mid > n){
+        }else{
             end = mid-1;
         }
        }
-       return 0;
+       double incr = 0.1;
+       for(int i = 0 ; i<p;i++){
+        while (root*root<=n){
+            root += incr;
+        }
+        root-= incr;
+        incr /=10;
+       }
+       return root;
     }
 }
