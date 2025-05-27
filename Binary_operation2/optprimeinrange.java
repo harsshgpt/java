5a3711e42d66087a671f78eff9576d2234456198
harsshgpt <harshgupta1752003@gmail.com>
2025-05-27 20:45:35 +0530

    format

--- Binary_operation2/optprimeinrange.java
+++ Binary_operation2/optprimeinrange.java
@@ -2,22 +2,23 @@ package Binary_operation2;
 
 public class optprimeinrange {
     public static void main(String[] args) {
-        int n= 10;
-        boolean [] primes = new boolean[n+1];
+        int n = 10;
+        boolean[] primes = new boolean[n + 1];
         seive(n, primes);
     }
-    //false in array number is prime
-    static void seive(int n , boolean[]primes){
-        for(int i = 2; i<=n;i++){
-            if(!primes[i]){
-              for(int j=i*2;j<=n;j +=i ){
-                primes[j] = true;
-              }
+
+    // false in array number is prime
+    static void seive(int n, boolean[] primes) {
+        for (int i = 2; i <= n; i++) {
+            if (!primes[i]) {
+                for (int j = i * 2; j <= n; j += i) {
+                    primes[j] = true;
+                }
             }
         }
-        for(int  i = 2; i<=n;i++){
-            if(!primes[i]){
-                System.out.print(i +" ");
+        for (int i = 2; i <= n; i++) {
+            if (!primes[i]) {
+                System.out.print(i + " ");
             }
         }
     }
