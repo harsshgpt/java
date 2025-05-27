32bc2e7a5f6c87042dbf58058a6b4bb91c780c8c
harsshgpt <harshgupta1752003@gmail.com>
2025-05-27 20:44:45 +0530

    s

--- Binary_operation2/gcd_lcm.java
+++ Binary_operation2/gcd_lcm.java
@@ -4,6 +4,7 @@ public class gcd_lcm {
     public static void main(String[] args) {
         System.out.println(lcm(2,7));
     }
+    
     static int gcd(int a ,int b ){
         if(a == 0){
             return b ;
