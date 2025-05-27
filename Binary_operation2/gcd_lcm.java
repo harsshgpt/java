7629bbedc7744e29ab44ddcc1f7e396e8a400943
harsshgpt <harshgupta1752003@gmail.com>
2025-05-27 20:45:14 +0530

    formatted

--- Binary_operation2/gcd_lcm.java
+++ Binary_operation2/gcd_lcm.java
@@ -2,18 +2,19 @@ package Binary_operation2;
 
 public class gcd_lcm {
     public static void main(String[] args) {
-        System.out.println(lcm(2,7));
+        System.out.println(lcm(2, 7));
     }
 
-    static int gcd(int a ,int b ){
-        if(a == 0){
-            return b ;
+    static int gcd(int a, int b) {
+        if (a == 0) {
+            return b;
         }
-        
-        return gcd(b%a,a);
+
+        return gcd(b % a, a);
     }
-    static int lcm (int a,int b){
-        return (a*b)/gcd(a, b);
+
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
     }
-    
+
 }
